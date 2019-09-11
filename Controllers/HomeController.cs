cc7c4de7afe07101151f14919101cf6c306bce42
Nick <nn605016@ohio.edu>
2019-09-11 11:19:41 -0400

    First set of changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Nankivell_MIS4200.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Development Journal";
 
             return View();
         }
---
+++ Models/customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nankivell_MIS4200.Models
+{
+    public class customer
+    {
+    }
+}
