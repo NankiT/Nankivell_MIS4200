70d6f786456792645062c03a65ae342a1e2a95ba
Nick <nn605016@ohio.edu>
2019-09-11 11:39:10 -0400

    changes made at end of class 9/11/2019

--- Models/customer.cs
+++ Models/customer.cs
@@ -7,5 +7,12 @@ namespace Nankivell_MIS4200.Models
 {
     public class customer
     {
+        public int customerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public ICollection<order> orders { get; set; }
+
     }
 }
---
+++ Models/order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nankivell_MIS4200.Models
+{
+    public class order
+    {
+        public int orderID { get; set; }
+        public int customerID { get; set; }
+        public virtual customer customer { get; set; }
+        public DateTime orderDate { get; set; }
+
+    }
+}
